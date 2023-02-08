93084a736e15a61bf7469852c37175f5e3b58024
DavidMoralesLuis <dmoralesluis@danielcastelao.org>
2023-02-08 11:36:35 +0100

    graficos_8feb

--- Graficos/primerEjemplo.py
+++ Graficos/primerEjemplo.py
@@ -1,6 +1,7 @@
 
 from reportlab.graphics.charts.barcharts import VerticalBarChart
 from reportlab.graphics.charts.piecharts import Pie
+from reportlab.graphics.charts.legends import Legend
 from reportlab.graphics.shapes import Drawing
 from reportlab.lib import colors
 from reportlab.platypus import SimpleDocTemplate, Spacer
@@ -48,16 +49,47 @@ gTarta.labels = ['Edge', 'Brave','Safari', 'Firefox', 'Chrome']
 gTarta.slices.strokeWidth = 0.5
 gTarta.slices[3].popout = 10
 gTarta.slices[3].strokeWidth = 2
-gTarta.slices[3].strokeDashArray = [2,2]
+gTarta.slices[3].strokeDashArray = [8,2]
 gTarta.slices[3].labelRadius = 1.75
 gTarta.slices[3].fontColor = colors.red
 gTarta.sideLabels = 1
 
 d.add(gTarta)
+
+
+lenda = Legend()
+lenda.x = 370
+lenda.y = 0
+lenda.dx = 8
+lenda.dy = 8
+lenda.fontName = 'Helvetica'
+lenda.fontSize = 7
+lenda.boxAnchor = 'n'
+lenda.columnMaximum = 10
+lenda.strokeWidth = 1
+lenda.strokeColor = colors.black
+lenda.deltax = 75
+lenda.deltay = 10
+lenda.autoXPadding = 5
+lenda.yGap = 0
+lenda.dxTextSpace = 5
+lenda.alignment = 'right'
+lenda.dividerLines = 1|2|4
+lenda.dividerOffsY = 4.5
+lenda.subCols.rpad = 30
+
+colores = [colors.blue,colors.red,colors.green,colors.yellow,colors.pink]
+for i, color in enumerate(colores):
+    gTarta.slices[i].fillColor = color
+
+lenda.colorNamePairs = [(
+    gTarta.slices[i].fillColor,
+    (gTarta.labels[i][0:20], '%0.2f' % gTarta.data[i]))
+    for i in range(len(gTarta.data))
+]
+
+d.add(lenda)
 guion.append(d)
-
-
-
 
 doc.build(guion)
 
